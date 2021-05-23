e6119fe7235e77f2ea1e4a504ad05289e40475c9
sanchit2843 <sanchittanwar75@gmail.com>
2021-05-23 08:16:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 pip install awscli
+pip install -U opencv-python
+sudo apt-get upgrade
+sudo apt install -y libsm6 libxext6 ffmpeg libfontconfig1 libxrender1 libgl1-mesa-glx
 aws configure set s3.max_concurrent_requests 100
-sudo su
-while sleep 2; do sync; echo 3 > /proc/sys/vm/drop_caches; done
